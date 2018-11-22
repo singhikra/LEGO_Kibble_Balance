03d41c34683f609576ab05471f1600e2d73c5a95
Ikram Singh <ikram.singh@callaghaninnovation.govt.nz>
2018-11-23 10:56:59 +1300

    Created Initialisation Methods

---
+++ LEGO_Kibble_Balance.py
@@ -0,0 +1,15 @@
+'''
+Program that informs the user the mass of an object placed on the LEGO Kibble Balance.
+'''
+
+def initialisePhidget():
+    pass
+
+def initialiseLabJackU6():
+    pass
+
+# Performs the initialisation of Hardware Components.
+def initialise():
+    initialisePhidget()
+    initialiseLabJackU6()
+
