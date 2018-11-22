1a1f7dd3aa03a5339481311e586023dab520622a
Ryan Curry <r.curry@callaghaninnovation.govt.nz>
2018-11-23 11:44:24 +1300

    Add test code to LEGO_Kibble_Balance.py

--- LEGO_Kibble_Balance.py
+++ LEGO_Kibble_Balance.py
@@ -1,9 +1,10 @@
 '''
 Program that informs the user the mass of an object placed on the LEGO Kibble Balance.
 '''
+import Phidget
 
 def initialisePhidget():
-    pass
+    Phidget.initialise()
 
 def initialiseLabJackU6():
     pass
@@ -13,3 +14,14 @@ def initialise():
     initialisePhidget()
     initialiseLabJackU6()
 
+def main():
+	try:
+		initialise()
+		while True:
+			Phidget.setVoltage(1.225, 0)
+	finally:
+		Phidget.close()
+
+
+if __name__=="__main__":
+	main()
