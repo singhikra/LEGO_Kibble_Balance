cf1990d667004ebda84f2ac6737e8808f63c4949
singhikra <ikram.singh@callaghaninnovation.govt.nz>
2018-11-26 16:49:12 +1300

    Added Conditions to prevent Phidget from exceeding output current fo 20mA

--- Main_Program.py
+++ Main_Program.py
@@ -4,9 +4,11 @@ import Phidget
 import LabJack_U6
 
 class LEGOKibbleBalance(LEGOKibbleBalanceGUI.LEGOKibbleBalance):
-	numChannels = 2  # Number of AIN channels being used
-	latestAinValues = [0] * numChannels
+	numChannels = 2  # Number of Aanalog Input channels being used
+	latestAinValues = [0] * numChannels  # Array for storing Voltages for each channel being used
 
+	''' Object initialiser for the main program. Also sets the initial conditions for the supply voltages 
+	for each coil '''
 	def __init__(self, parent):
 		LEGOKibbleBalanceGUI.LEGOKibbleBalance.__init__(self, parent)
 		self.KibbleLog.AppendText("Welcome to LEGO Kibble Balance!\n")
@@ -14,6 +16,7 @@ class LEGOKibbleBalance(LEGOKibbleBalanceGUI.LEGOKibbleBalance):
 		self.CoilASupplyVoltage.SetValue("0.0")
 		self.CoilBSupplyVoltage.SetValue("0.0")
 
+	''' Communicate with LabJack U6 and get the voltages across the resistor in series with each coil '''
 	def GetCoilVoltages(self):
 		global numChannels, latestAinValues
 		numChannels = 2  # Number of AIN channels being used
@@ -54,30 +57,47 @@ class LEGOKibbleBalance(LEGOKibbleBalanceGUI.LEGOKibbleBalance):
 		finally:
 			d.close()
 
+	''' Display the voltages across each resistor in the GUI '''
 	def DisplayResVoltages(self):
 		self.VoltageAcrossResA.SetValue(str(latestAinValues[0]))
 		self.VoltageAcrossResB.SetValue(str(latestAinValues[1]))
 
+	''' Display the current through each resistor in the GUI '''
 	def DisplayCoilCurrents(self):
 		self.CurrentThroughCoilA.SetValue(str(latestAinValues[0]/178.8))
 		self.CurrentThroughCoilB.SetValue(str(latestAinValues[1]/178.9))
 
+	''' Set the supply voltage for Coil A '''
 	def SetCoilAVoltageOnButtonClick(self, event):
 		self.KibbleLog.AppendText("Setting Coil A Supply Voltage to " + self.CoilASupplyVoltage.GetValue() + " V\n")
 		Supply_Voltage_A = float(self.CoilASupplyVoltage.GetValue())
-		Phidget.setVoltage(Supply_Voltage_A, 0)
+		# Conditions to prevent Phidget from exceeding output current limit for Coil A
+		if (Supply_Voltage_A > 8):
+			Phidget.setVoltage(8, 0)
+		elif (Supply_Voltage_A < -8):
+			Phidget.setVoltage(-8, 0)
+		else:
+			Phidget.setVoltage(Supply_Voltage_A, 0)
 		self.GetCoilVoltages()
 		self.DisplayCoilCurrents()
 		self.DisplayResVoltages()
 
+	''' Set the supply voltage for Coil B '''
 	def SetCoilBVoltageOnButtonClick(self, event):
 		self.KibbleLog.AppendText("Setting Coil B Supply Voltage to " + self.CoilBSupplyVoltage.GetValue() + " V\n")
 		Supply_Voltage_B = float(self.CoilBSupplyVoltage.GetValue())
-		Phidget.setVoltage(Supply_Voltage_B, 1)
+		# Conditions to prevent Phidget from exceeding output current limit for Coil A
+		if(Supply_Voltage_B>8):
+			Phidget.setVoltage(8, 1)
+		elif(Supply_Voltage_B<-8):
+			Phidget.setVoltage(-8, 1)
+		else:
+			Phidget.setVoltage(Supply_Voltage_B, 1)
 		self.GetCoilVoltages()
 		self.DisplayCoilCurrents()
 		self.DisplayResVoltages()
 
+	''' Close the program when user selects 'x' on the window '''
 	def LEGOKibbleBalanceOnClose(self, event):
 		Phidget.close()
 		self.Show(False)
