5783f8e555d6f8ba8bac2c3d51b8bd246bbf600c
singhikra <ikram.singh@callaghaninnovation.govt.nz>
2018-11-26 14:00:25 +1300

    Updated GUI for LEGO Kibble Balance. Updated Functions for GUI.

--- LEGOKibbleBalanceGUI.py
+++ LEGOKibbleBalanceGUI.py
@@ -17,7 +17,7 @@ import wx.xrc
 class LEGOKibbleBalance ( wx.Frame ):
 	
 	def __init__( self, parent ):
-		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"Lego Kibble Balance", pos = wx.DefaultPosition, size = wx.Size( 353,429 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"LEGO Kibble Balance", pos = wx.DefaultPosition, size = wx.Size( 353,429 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
 		
 		self.SetSizeHintsSz( wx.DefaultSize, wx.DefaultSize )
 		
@@ -25,7 +25,7 @@ class LEGOKibbleBalance ( wx.Frame ):
 		fgSizer1.SetFlexibleDirection( wx.BOTH )
 		fgSizer1.SetNonFlexibleGrowMode( wx.FLEX_GROWMODE_SPECIFIED )
 		
-		self.m_staticText1 = wx.StaticText( self, wx.ID_ANY, u"Voltage", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText1 = wx.StaticText( self, wx.ID_ANY, u"Supply Voltages", wx.DefaultPosition, wx.DefaultSize, 0 )
 		self.m_staticText1.Wrap( -1 )
 		fgSizer1.Add( self.m_staticText1, 0, wx.ALL, 5 )
 		
@@ -33,24 +33,78 @@ class LEGOKibbleBalance ( wx.Frame ):
 		self.m_staticText2.Wrap( -1 )
 		fgSizer1.Add( self.m_staticText2, 0, wx.ALL, 5 )
 		
-		self.m_textCtrl1 = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
-		fgSizer1.Add( self.m_textCtrl1, 0, wx.ALL, 5 )
+		self.CoilASupplyVoltage = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+		fgSizer1.Add( self.CoilASupplyVoltage, 0, wx.ALL, 5 )
 		
-		self.SetCoilAVoltage = wx.Button( self, wx.ID_ANY, u"Set Coil A Voltage", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.SetCoilAVoltage = wx.Button( self, wx.ID_ANY, u"Set Coil A Supply Voltage", wx.DefaultPosition, wx.DefaultSize, 0 )
 		fgSizer1.Add( self.SetCoilAVoltage, 0, wx.ALL, 5 )
 		
-		self.m_textCtrl2 = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
-		fgSizer1.Add( self.m_textCtrl2, 0, wx.ALL, 5 )
+		self.CoilBSupplyVoltage = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+		fgSizer1.Add( self.CoilBSupplyVoltage, 0, wx.ALL, 5 )
 		
-		self.SetCoilBVoltage = wx.Button( self, wx.ID_ANY, u"Set Coil B Voltage", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.SetCoilBVoltage = wx.Button( self, wx.ID_ANY, u"Set Coil B Supply Voltage", wx.DefaultPosition, wx.DefaultSize, 0 )
 		fgSizer1.Add( self.SetCoilBVoltage, 0, wx.ALL, 5 )
-
+		
+		self.m_staticText6 = wx.StaticText( self, wx.ID_ANY, u"Coil Currents", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText6.Wrap( -1 )
+		fgSizer1.Add( self.m_staticText6, 0, wx.ALL, 5 )
+		
+		self.m_staticText7 = wx.StaticText( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText7.Wrap( -1 )
+		fgSizer1.Add( self.m_staticText7, 0, wx.ALL, 5 )
+		
+		self.CurrentThroughCoilA = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.CurrentThroughCoilA.Enable( False )
+		
+		fgSizer1.Add( self.CurrentThroughCoilA, 0, wx.ALL, 5 )
+		
+		self.m_staticText9 = wx.StaticText( self, wx.ID_ANY, u"Current Through Coil A", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText9.Wrap( -1 )
+		fgSizer1.Add( self.m_staticText9, 0, wx.ALL, 5 )
+		
+		self.CurrentThroughCoilB = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.CurrentThroughCoilB.Enable( False )
+		
+		fgSizer1.Add( self.CurrentThroughCoilB, 0, wx.ALL, 5 )
+		
+		self.m_staticText10 = wx.StaticText( self, wx.ID_ANY, u"Current Through Coil B", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText10.Wrap( -1 )
+		fgSizer1.Add( self.m_staticText10, 0, wx.ALL, 5 )
+		
+		self.m_staticText3 = wx.StaticText( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText3.Wrap( -1 )
+		fgSizer1.Add( self.m_staticText3, 0, wx.ALL, 5 )
+		
+		self.m_staticText4 = wx.StaticText( self, wx.ID_ANY, u"Kibble Log", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText4.Wrap( -1 )
+		fgSizer1.Add( self.m_staticText4, 0, wx.ALL, 5 )
+		
+		self.m_staticText5 = wx.StaticText( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText5.Wrap( -1 )
+		fgSizer1.Add( self.m_staticText5, 0, wx.ALL, 5 )
+		
+		self.KibbleLog = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( 200,100 ), wx.TE_MULTILINE )
+		fgSizer1.Add( self.KibbleLog, 0, wx.ALL, 5 )
+		
+		
 		self.SetSizer( fgSizer1 )
 		self.Layout()
 		
 		self.Centre( wx.BOTH )
+		
+		# Connect Events
+		self.SetCoilAVoltage.Bind( wx.EVT_BUTTON, self.SetCoilAVoltageOnButtonClick )
+		self.SetCoilBVoltage.Bind( wx.EVT_BUTTON, self.SetCoilBVoltageOnButtonClick )
 	
 	def __del__( self ):
 		pass
+	
+	
+	# Virtual event handlers, overide them in your derived class
+	def SetCoilAVoltageOnButtonClick( self, event ):
+		event.Skip()
+	
+	def SetCoilBVoltageOnButtonClick( self, event ):
+		event.Skip()
 	
 
--- Main_Program.py
+++ Main_Program.py
@@ -1,10 +1,24 @@
 import LEGOKibbleBalanceGUI
 import wx
+import Phidget
 
 class LEGOKibbleBalance(LEGOKibbleBalanceGUI.LEGOKibbleBalance):
 
 	def __init__(self, parent):
 		LEGOKibbleBalanceGUI.LEGOKibbleBalance.__init__(self, parent)
+		self.KibbleLog.AppendText("Welcome to LEGO Kibble Balance!\n")
+		self.CoilASupplyVoltage.SetValue("0.0")
+		self.CoilBSupplyVoltage.SetValue("0.0")
+
+	def SetCoilAVoltageOnButtonClick(self, event):
+		self.KibbleLog.AppendText("Setting Coil A Supply Voltage to " + self.CoilASupplyVoltage.GetValue() + " V\n")
+		Supply_Voltage_A = float(self.CoilASupplyVoltage.GetValue())
+		Phidget.setVoltage(Supply_Voltage_A, 0)
+
+	def SetCoilBVoltageOnButtonClick(self, event):
+		self.KibbleLog.AppendText("Setting Coil B Supply Voltage to " + self.CoilBSupplyVoltage.GetValue() + " V\n")
+		Supply_Voltage_B = float(self.CoilBSupplyVoltage.GetValue())
+		Phidget.setVoltage(Supply_Voltage_B, 1)
 
 
 # mandatory in wx, create an app, False stands for not deteriction stdin/stdout
